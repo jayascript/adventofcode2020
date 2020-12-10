79b50ff847bb1fd3be0ba42ed3ed18c594db9b1b
Jayascript <6750308-jayascript@users.noreply.gitlab.com>
2020-12-10 12:21:40 -0500

    Complete day 2

--- 2020/2/code.py
+++ 2020/2/code.py
@@ -2,13 +2,40 @@
 # Author = Jayascript
 # Date = December 2020
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+import re
+import numpy as np
+import pandas as pd
 
 
+df = pd.read_csv("input.txt", sep=" ", header=None)
+df = df.rename(columns={
+    0:'Freqs',
+    1:'Char',
+    2:'Pass',
+})
 
-print("Part One : "+ str(None))
+df['Char'] = df['Char'].apply(lambda x: x.replace(':', ''))
+
+df[['First', 'Second']] = df['Freqs'].str.split(
+    '-', 1, expand=True)
+
+df['First'] = df['First'].astype('int')
+df['Second'] = df['Second'].astype('int')
+
+count = 0
+for index, row in df.iterrows():
+    result = len(re.findall(row['Char'], row['Pass']))
+    if row['First'] <= result <= row['Second']:
+        count += 1
 
 
+print("Part One : "+ str(count))
 
-print("Part Two : "+ str(None))
+count = 0
+for index, row in df.iterrows():
+    a = row['Pass'][row['First'] - 1] == row['Char']
+    b = row['Pass'][row['Second'] - 1] == row['Char']
+    if bool(a) ^ bool(b):
+        count +=1
+
+print("Part Two : "+ str(count))
